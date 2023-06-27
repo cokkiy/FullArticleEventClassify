93c418d7c0ef93b4612f3b6add4837781adbf164
Cokkiy Zhang <cokkiy2001@hotmail.com>
2023-06-27 21:00:37 +0800

    add freeze_bert params

--- EventExtratorClassifer.py
+++ EventExtratorClassifer.py
@@ -4,14 +4,19 @@ from transformers import BertModel
 
 
 class EventExtractorClassifer(nn.Module):
-    def __init__(self, bert_model, num_labels):
+    def __init__(self, bert_model, num_labels, freeze_bert=False):
         super(EventExtractorClassifer, self).__init__()
         self.bert = bert_model
         self.dropout = nn.Dropout(0.3)
         self.classifier = nn.Linear(self.bert.config.hidden_size, num_labels)
 
-    def forward(self, input_ids, attention_mask):
-        outputs = self.bert(input_ids, attention_mask=attention_mask)
-        pooled_output = outputs[1]
+        if freeze_bert:
+            for param in self.bert.parameters():
+                param.requires_grad = False
+
+    def forward(self, input_ids, attention_mask=None):
+        outputs = self.bert(
+            input_ids, attention_mask=attention_mask, return_dict=False)
+        pooled_output = outputs[1][-1]
         pooled_output = self.dropout(pooled_output)
         return self.classifier(pooled_output)
