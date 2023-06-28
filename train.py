aa1653477df1c25058b2781178b57f69075e69bc
Cokkiy Zhang <cokkiy2001@hotmail.com>
2023-06-28 22:50:54 +0800

    add start arguments to train

--- train.py
+++ train.py
@@ -9,27 +9,58 @@ from event_dataset import EventDataset
 from arguments_dataset import ArgumentsDataset
 
 
+import argparse
+import os
+
+# get current timestamp and convert to string
+timestamp = str(int(time.time()))
+
+parser = argparse.ArgumentParser(
+    description='Train model on the FNDEE dataset')
+parser.add_argument('--path', type=str, default='../result/models',
+                    help='The file path will save the trained model (default: ../result/models)')
+parser.add_argument('--model_name', type=str, default=f'{timestamp}',
+                    help='The file name of the trained model will be saved (default: timestamp)')
+parser.add_argument('--train_file', type=str, default='./data/FNDEE_train1.json',
+                    help='The file name of the train dateset file')
+parser.add_argument('--batch_size', type=int,
+                    help='Train Batch size, default 4', default=4)
+parser.add_argument('--bert_model', type=str, default='../models/xlm-roberta-base',
+                    help='Pretrained bert model name (default: "../models/xlm-roberta-base")')
+parser.add_argument('--num_epochs', type=int, default=20,
+                    help='Number of epochs to train (default: 20)')
+
+args = parser.parse_args()
+
+path = args.path
+model_name = args.model_name
+train_file = args.train_file
+batch_size = args.batch_size
+bert_name = args.bert_model
+num_epochs = args.num_epochs
+
+if not os.path.exists(path):
+    os.makedirs(path)
+
 # 设置超参数
 num_event_types = 9  # 事件类型数量
 num_arguments = 12  # 论元分类数量
 learning_rate = 1e-5
-num_epochs = 30
-batch_size = 4  # 16
+
 
 # device specific
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # 加载预训练的Bert模型和分词器
-tokenizer = AutoTokenizer.from_pretrained('../models/xlm-roberta-base')
-config = AutoConfig.from_pretrained(
-    "../models/xlm-roberta-base", output_hidden_states=True)
+print("Loading BERT model...")
+tokenizer = AutoTokenizer.from_pretrained(bert_name)
+config = AutoConfig.from_pretrained(bert_name, output_hidden_states=True)
 model = AutoModelForMaskedLM.from_pretrained(
-    "../models/xlm-roberta-base", config=config).to(device)
+    bert_name, config=config).to(device)
 
 # 训练数据
-train_args_dataset = ArgumentsDataset(
-    './data//FNDEE_train1.json', tokenizer)  # 论元数据集
-train_event_dataset = EventDataset(
-    './data//FNDEE_train1.json', tokenizer)  # 事件数据集
+print("Loading training data...")
+train_args_dataset = ArgumentsDataset(train_file, tokenizer)  # 论元数据集
+train_event_dataset = EventDataset(train_file, tokenizer)  # 事件数据集
 # Dataloader
 train_event_dataloader = DataLoader(
     train_event_dataset, batch_size=batch_size, collate_fn=train_event_dataset.pad_collate_fn, shuffle=False)
@@ -48,6 +79,7 @@ event_optimizer = torch.optim.AdamW(
     event_model.parameters(), lr=learning_rate, eps=1e-8)
 
 # Training loop for event extraction and classification training
+print("Training event extraction and classification model...")
 event_type_criterion = nn.CrossEntropyLoss()
 for epoch in range(num_epochs):
     event_model.train()
@@ -68,6 +100,7 @@ for epoch in range(num_epochs):
     print(f'Epoch: {epoch+1}, Training Loss: {avg_train_loss}')
 
 
+print("Training arguments extraction and classification model...")
 # Optimizer
 args_optimizer = torch.optim.AdamW(
     args_model.parameters(), lr=learning_rate, eps=1e-8)
@@ -90,44 +123,9 @@ for epoch in range(num_epochs):
     avg_train_loss = total_loss / len(train_args_dataloader)
     print(f'Epoch: {epoch+1}, Training Loss: {avg_train_loss}')
 
-# get current timestamp and convert to string
-timestamp = str(int(time.time()))
-# create file name using timestamp
-event_filename = f"../result/models/event_model_{timestamp}"
-args_filename = f"../result/models/args_model_{timestamp}"
+print("Saving model...")
+event_filename = f"{path}/{model_name}-event.pt"
+args_filename = f"{path}/{model_name}-args.pt"
 torch.save(event_model.state_dict(), event_filename)
 torch.save(args_model.state_dict(), args_filename)
 print(f"model file {event_filename} and {args_filename} saved")
-
-# Evaluation loop
-# model.eval()
-# with torch.no_grad():
-#     eval_loss = 0
-#     for batch in valid_dataloader:
-#         b_input_ids, b_input_mask, b_labels = tuple(
-#             t.to(device) for t in batch)
-#         loss = model(b_input_ids, token_type_ids=None,
-#                      attention_mask=b_input_mask, labels=b_labels)
-#         eval_loss += loss.item()
-#     avg_eval_loss = eval_loss / len(valid_dataloader)
-#     print(f'Validation Loss: {avg_eval_loss}')
-
-
-# # 使用训练好的模型进行预测（假设有测试数据）
-# test_data = ...
-# predictions = []
-
-# for data in test_data:
-#     text = data['text']
-#     encoded = tokenizer.encode(text, add_special_tokens=True)
-#     input_ids = torch.tensor(encoded).unsqueeze(0)
-
-#     event_type_logits, argument_logits = extractor(input_ids)
-
-#     predicted_event_type = torch.argmax(event_type_logits, dim=1).item()
-#     predicted_argument = torch.argmax(argument_logits, dim=1).item()
-
-#     predictions.append({'event_type': predicted_event_type,
-#                        'argument': predicted_argument})
-
-# print(predictions)
