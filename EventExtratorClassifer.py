2ae6c8f15272a568a5d2f92b67c7daf058d32336
Cokkiy Zhang <cokkiy2001@hotmail.com>
2023-07-11 13:05:24 +0800

    事件提取模型现在合并最后一层和倒数第二层数据，共同输出

--- EventExtratorClassifer.py
+++ EventExtratorClassifer.py
@@ -8,7 +8,7 @@ class EventExtractorClassifer(nn.Module):
         super(EventExtractorClassifer, self).__init__()
         self.bert = bert_model
         self.dropout = nn.Dropout(0.3)
-        self.classifier = nn.Linear(self.bert.config.hidden_size, num_labels)
+        self.classifier = nn.Linear(self.bert.config.hidden_size * 2, num_labels)
 
         if freeze_bert:
             for param in self.bert.parameters():
@@ -16,8 +16,11 @@ class EventExtractorClassifer(nn.Module):
 
     def forward(self, input_ids, attention_mask=None):
         output = self.bert(input_ids, attention_mask)
-        pooled_output = output[1][-1]
-        pooled_output = self.dropout(pooled_output)
+        pooled_output1 = output["hidden_states"][-1]
+        pooled_output2 = output["hidden_states"][-2]
+        pooled_output = self.dropout(
+            torch.cat((pooled_output1, pooled_output2), dim=-1)
+        )
         return self.classifier(pooled_output)
 
     def save_pretrained(self, path, save_function=None):
