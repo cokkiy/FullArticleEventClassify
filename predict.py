160b8c621f7253d7db9f4f7867bee7010edfaa54
Cokkiy Zhang <cokkiy2001@hotmail.com>
2023-07-13 21:08:11 +0800

    finished combination

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ import argparse
 import json
 import os
 import time
+import numpy as np
 import torch
 from transformers import (
     AutoTokenizer,
@@ -17,7 +18,7 @@ from EventExtratorClassifer import EventExtractorClassifer
 from arguments_dataset2 import ArgumentsDataset
 from event_dataset2 import EventDataset
 from datasets import load_dataset
-from commonfn import convert_tokenoffset_to_charoffset
+from commonfn import convert_tokenoffset_to_charoffset, get_token_index
 from GlobalExtractor import EffiGlobalExtractor as GlobalExtractor
 
 parser = argparse.ArgumentParser(description="Predict test data with trained model")
@@ -53,9 +54,23 @@ event_bert_name = "../models/roberta_wwm"
 bert_ner_name = "../models/xlm-roberta-base-ner-hrl"
 bert_name = "../models/xlm-roberta-base"
 
+ent2id, id2ent = {
+    "Non-event": 0,
+    "Experiment": 1,
+    "Manoeuvre": 2,
+    "Deploy": 3,
+    "Support": 4,
+    "Accident": 5,
+    "Exhibit": 6,
+    "Conflict": 7,
+    "Injure": 8,
+}, {}
+
+for k, v in ent2id.items():
+    id2ent[v] = k
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-config = AutoConfig.from_pretrained(bert_ner_name, output_hidden_states=True)
 bert_model = BertModel.from_pretrained(event_bert_name).to(device)
 tokenizer = AutoTokenizer.from_pretrained(event_bert_name)
 
@@ -65,7 +80,7 @@ ENT_CLS_NUM = 9
 ARGUMENT_CLS_NUM = 11
 
 # events prediction model
-event_model = GlobalExtractor(bert_model, ENT_CLS_NUM, ARGUMENT_CLS_NUM, 64).to(device)
+event_model = GlobalExtractor(bert_model, ENT_CLS_NUM, ARGUMENT_CLS_NUM, 128).to(device)
 event_model.load_state_dict(
     torch.load(
         event_model_name,
@@ -74,6 +89,7 @@ event_model.load_state_dict(
 )
 
 # arguments prediction model
+config = AutoConfig.from_pretrained(bert_ner_name, output_hidden_states=True)
 bert_ner_model_args = AutoModelForTokenClassification.from_pretrained(
     bert_ner_name, config=config
 ).to(device)
@@ -81,6 +97,8 @@ config = AutoConfig.from_pretrained(bert_name, output_hidden_states=True)
 bert_model_args = AutoModelForMaskedLM.from_pretrained(bert_name, config=config).to(
     device
 )
+
+args_tokenizer = AutoTokenizer.from_pretrained(bert_name)
 args_model = ArgumentsExtratorClassifer(
     bert_ner_model_args, bert_model=bert_model_args, num_labels=num_args_types
 ).to(device)
@@ -100,69 +118,68 @@ for sample in dataset["train"]:
     event_inputs = tokenizer(text, return_offsets_mapping=True, return_tensors="pt").to(
         device
     )
-    outputs = event_model(event_inputs["input_ids"], event_inputs["attention_mask"])
-    predictions = outputs.argmax(dim=-1)
-    tokens = tokenizer.convert_ids_to_tokens(
-        event_inputs["input_ids"].squeeze(),
-        skip_special_tokens=True,
+    outputs, _ = event_model(
+        event_inputs["input_ids"],
+        event_inputs["attention_mask"],
+        event_inputs["token_type_ids"],
     )
-    predicted_labels = [
-        EventDataset.LabelNames[prediction.item()] for prediction in predictions[0]
-    ]
-    # print(" ".join([f"{token}/{label}" for token, label in zip(tokens, predicted_labels)]))
-    labeld_tokens = [
-        (idx, predicted_labels[idx], tokens[idx])
-        for idx in range(len(tokens))
-        if predicted_labels[idx] != "O"
-    ]
+    event_offsets = convert_tokenoffset_to_charoffset(
+        event_inputs["offset_mapping"][0].data.cpu().numpy()
+    )
 
-    merged_tokens = []
-    for labeld_token in labeld_tokens:
-        idx, t, trigger = labeld_token
-        if t.startswith("B-"):
-            merged_tokens.append((idx, t, trigger))
-        elif t.startswith("I-") and len(merged_tokens) > 0:
-            merged_tokens[-1] = (
-                merged_tokens[-1][0],
-                merged_tokens[-1][1],
-                merged_tokens[-1][2] + trigger,
-            )
+    scores = outputs[0].data.cpu().numpy()
+    scores[:, :, [0, -1]] -= np.inf
+    event_info_list = []
 
-    event_offsets = convert_tokenoffset_to_charoffset(event_inputs["offset_mapping"][0])
+    for label, start, end in zip(*np.where(scores > 0)):
+        event_type = id2ent[label.item()]
+        start_idx = 0 if label == 0 else event_offsets[start][0]
+        end_idx = 0 if label == 0 else event_offsets[end][-1]
+        event_info = {
+            "start_idx": start_idx,
+            "end_idx": end_idx,
+            "event_type": event_type,
+            "text": text[start_idx:end_idx],
+        }
+        event_info_list.append(event_info)
+
     # event list
     event_list = []
     event = {}
+    tokens = args_tokenizer.tokenize(text)
 
     trigger_text = ""
     trigger_position = 0
-    for idx, label, token in merged_tokens:
+    for event_info in event_info_list:
+        start_idx, end_idx, label, token = (
+            event_info["start_idx"],
+            event_info["end_idx"],
+            event_info["event_type"],
+            event_info["text"],
+        )
         trigger_text = token
-        trigger_position = idx
+        trigger_position = get_token_index(text, tokens, start_idx)
         # create the event node
         event = {
-            "event_type": label[2:],
+            "event_type": label,
             "trigger": {
                 "text": trigger_text,
-                "offset": [
-                    event_offsets[trigger_position][0].data.cpu().item()
-                    + 1,  # start offset
-                    event_offsets[trigger_position][0].data.cpu().item()
-                    + 1
-                    + len(trigger_text),  # end offfset
-                ],
+                "offset": [start_idx, end_idx],
             },
             "arguments": [],
         }
 
         # extract the arguments
         prompt_text = f"{trigger_text},位置{trigger_position}"
-        prompted_text = f"{tokenizer.cls_token}{prompt_text}{tokenizer.sep_token}{text}"
-        inputs = tokenizer(
+        prompted_text = (
+            f"{args_tokenizer.cls_token}{prompt_text}{args_tokenizer.sep_token}{text}"
+        )
+        inputs = args_tokenizer(
             prompted_text, return_offsets_mapping=True, return_tensors="pt"
         ).to(device)
         args_outputs = args_model(inputs["input_ids"], inputs["attention_mask"])
         args_predictions = args_outputs.argmax(dim=-1)
-        args_tokens = tokenizer.convert_ids_to_tokens(
+        args_tokens = args_tokenizer.convert_ids_to_tokens(
             inputs["input_ids"].squeeze(),
             # skip_special_tokens=True,
         )
@@ -181,7 +198,9 @@ for sample in dataset["train"]:
                 and args_predicted_labels[i + 1] == args_predicted_labels[i - 1]
             ):
                 args_predicted_labels[i] = args_predicted_labels[i - 1]
-        args_offsets = convert_tokenoffset_to_charoffset(inputs["offset_mapping"][0])
+        args_offsets = convert_tokenoffset_to_charoffset(
+            inputs["offset_mapping"][0].data.cpu().numpy()
+        )
         pre_label = "O"
         current_arg_text = ""
         current_arg_start_idx = 0
@@ -208,13 +227,13 @@ for sample in dataset["train"]:
                             "role": current_arg_type,
                             "text": current_arg_text,
                             "offset": [
-                                current_arg_start_idx.data.cpu().item()
+                                current_arg_start_idx
                                 - len(
                                     prompt_text
                                 )  # should minus len of prompt text because we need the offset in the original text
                                 - 2  # should minus 2 because of [CLS] and [SEP]
                                 - 5,  # don't know why, but it works
-                                current_arg_start_idx.data.cpu().item()
+                                current_arg_start_idx
                                 - len(prompt_text)
                                 - 2
                                 - 5
